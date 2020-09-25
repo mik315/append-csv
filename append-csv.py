14fd2573ac5b9e02b11127c3c30505d9d3af31a8
Miguel Ramirez <miguelramirez@Miguels-MacBook-Pro.local>
2020-09-25 17:58:04 -0500

    clean up

--- append-csv.py
+++ append-csv.py
@@ -1,6 +1,7 @@
 # Append as many CSV files are in a directory into a single csv file.
 
-import os, fnmatch
+import os
+import fnmatch
 import pandas as pd
 
 ls_files = os.listdir('.')
@@ -9,20 +10,20 @@ extension = "*.csv"
 read_rows = 0
 sum_rows = 0
 
-for read_file in ls_files :
+for read_file in ls_files:
 
-    if read_file == 'append-csv.py' :
+    if read_file == 'append-csv.py':
         None
 
-    elif fnmatch.fnmatch(read_file, extension) :
+    elif fnmatch.fnmatch(read_file, extension):
         read_rows = pd.read_csv(read_file, dtype=str, index_col=0)
         read_rows.to_csv('NCDC-all.csv', mode='a', header=False)
-        rows =len(read_rows)
+        rows = len(read_rows)
         sum_rows = rows + sum_rows
-        print('Appending ' +str(rows), "rows from file: ", read_file, "to the dataset...")
+        print('Appending '+str(rows), "rows from file: ", read_file, "to the dataset...")
 
-    else :
+    else:
         print("Rows from other files:", read_file, "was not appended, does not have csv extension.")
 
 print("")
-print("Total rows appended to the dataset: " +str(sum_rows))
+print("Total rows appended to the dataset: "+str(sum_rows))
